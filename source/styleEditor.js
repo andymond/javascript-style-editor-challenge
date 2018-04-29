04158fd271de25cfcf167c0343bc70d2a193eaf4
Andrew Dymond <andy.dymond@gmail.com>
2018-04-29 13:55:48 -0600

    Add jquery to allow users to style page with form

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-  // your code goes here.
+  $('#style-submit').click(function(event) {
+    event.preventDefault();
+    const selector = $('input[name=selector]');
+    const property = $('input[name=property]');
+    const value    = $('input[name=value]');
+    $(selector.val()).css(property.val(), value.val())
+    console.log('you clicked the thing');
+  });
 });
